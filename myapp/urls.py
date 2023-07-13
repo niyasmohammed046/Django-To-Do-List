0dca2ea96c88e69dec89e318f31fb4555afee9ab
niyasmohammed046 <niyasmohammed046@gmail.com>
2023-07-13 18:12:22 +0530

    To Do app Crud Finished

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 
     path('',views.home,name='home'),
-    path('addtask/',views.addtask,name="addtask")
+    path('addtask/',views.addtask,name="addtask"),
+    path('done/<int:pk>/',views.MarkAsDone,name="MarkAsDone"),
+    path('undone/<int:pk>/',views.MarkAsUnDone,name="MarkAsUnDone"),
+    path('edit/<int:pk>/',views.edit,name="edit"),
+    path('delete/<int:pk>/',views.deletetask,name="delete_task")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
-
+from django.http import HttpResponse
 
 def home(request):
     task = Task.objects.filter(is_completed = False).order_by('-updated_at')
-    context = {'task':task}
+    completed_task = Task.objects.filter(is_completed=True)
+    # task_undone = Task.objects.filter(is_completed=False)
+
+    context = {
+        'task':task,
+        'completed_task':completed_task,
+        # 'task_undone':task_undone
+         }
     return render(request,'home.html',context)
 
 def addtask(request):
@@ -12,4 +19,34 @@ def addtask(request):
         addtask = request.POST["addingtask"]
         Task.objects.create(task=addtask)
         return redirect('home')
+    
+def MarkAsDone(request ,pk):
+    taskdone = get_object_or_404(Task,pk=pk)
+    taskdone.is_completed = True
+    taskdone.save()
+    return redirect('home')
 
+def MarkAsUnDone(request,pk):
+    taskundone = get_object_or_404(Task,pk=pk)
+    taskundone.is_completed= False
+    taskundone.save()
+    return redirect('home')
+
+def edit(request,pk):
+    gettask = get_object_or_404(Task,pk=pk)
+    if request.method == "POST":
+        new_task = request.POST['updating_task']
+        gettask.task = new_task
+        gettask.save()
+        return redirect(home)
+    else:
+        context = {
+            'gettask':gettask
+        }
+        print(gettask)
+        return render(request,'editpage.html',context)
+    
+def deletetask(request,pk):
+    task =get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect(home)
